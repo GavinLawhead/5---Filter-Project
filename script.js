acc70656d9017a90347192863a1474cae1105bf8
55746 <Lawhead.gavin@live.jobcorps.org>
2022-07-19 14:14:01 +0000

    working on it

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const numbers = [{ hardness: 11 }, { hardness: 29 }, 1, 13, 497];
-
+const Minerals = [
+  { gem: "Diamond", hardness: 1 },
+  { gem: "Ruby", hardness: 2 },
+  { gem: "Sapphire", hardness: 3 },
+  { gem: "Emerald", hardness: 4 },
+  { gem: "Amathyst", hardness: 5 },
+  { gem: "Amber", hardness: 6 },
+  { metal: "Iron", hardness: 7 },
+  { metal: "Steel", hardness: 8 },
+  { metal: "Nickel", hardness: 9 },
+  { metal: "Tin", hardness: 10 },
+  { metal: "Gold", hardness: 11 },
+];
 let keys = document.getElementById("searchinput");
 let but = document.getElementById("search");
 
 function searchbar() {
-  if (numbers.hasOwnProperty(Number(keys.value)) == -1) {
+  if (numbers.includes(Number(keys.value)) == false) {
     alert(keys.value + " Not Found");
   } else {
     alert(
-      "message shows it worked, mineral @ " +
-        numbers.indexOf(Number(keys.value)) +
-        " has that hardness"
+      "mineral @ " + numbers.indexOf(Number(keys.value)) + " has that hardness"
     );
   }
 }
