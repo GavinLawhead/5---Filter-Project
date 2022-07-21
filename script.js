1bbc793a2d0a036c11528f0176780143e0a4687a
55746 <Lawhead.gavin@live.jobcorps.org>
2022-07-21 15:25:45 +0000

    steady grind

--- script.js
+++ script.js
@@ -5,43 +5,26 @@ const Minerals = [
   { gem: "Emerald", hardness: 4 },
   { gem: "Amathyst", hardness: 5 },
   { gem: "Amber", hardness: 6 },
-  { metal: "Iron", hardness: 7 },
-  { metal: "Steel", hardness: 8 },
-  { metal: "Nickel", hardness: 9 },
-  { metal: "Tin", hardness: 10 },
-  { metal: "Gold", hardness: 11 },
+  // { metal: "Iron", hardness: 7 },
+  // { metal: "Steel", hardness: 8 },
+  // { metal: "Nickel", hardness: 9 },
+  // { metal: "Tin", hardness: 10 },
+  // { metal: "Gold", hardness: 11 },
 ];
-var results = [];
+let searchinput = document.querySelector("searchinput");
+// let therock = ;
 
-let keys = document.getElementById("searchinput");
-let searchbutton = document.getElementById("searchbutton");
-searchbutton.addEventListener("click", filterIt);
-function filterIt(arr, searchKey) {
-  Minerals = arr;
-  keys = searchKey;
-  return arr.filter((obj) =>
-    Object.keys(obj).some((key) => obj[key].includes(searchKey))
-  );
+function checkarray() {
+  let rock = [];
+  let therock = Minerals.find((rock) => rock.gem == searchinput);
+  for (let i = 0; i < Minerals.length; i++) {
+    rock += Minerals[i];
+    console.log(Minerals[i]);
+  }
+  if (Minerals.find((rock) => rock.gem == searchinput)) {
+    alert("this will display when the word is found");
+  }
+  if (searchinput != therock) {
+    alert("undefined text");
+  }
 }
-
-// const numbers = {
-//   hardness: 11,
-//   hardness: 29,
-//   hardness: 1,
-//   hardness: 13,
-//   hardness: 497,
-// };
-// document.innerHTML = numbers;
-
-// function checkKey() {
-//   if (
-//     numbers.toString().includes(document.getElementById("searchinput")) ===
-//     false
-//   ) {
-//     alert(keys.value + " Not Found");
-//   } else {
-//     alert(numbers.indexOf(Number(keys.value)) + " is in the array");
-//   }
-// }
-
-// checkKey(numbers, "hardness"); // Return true
