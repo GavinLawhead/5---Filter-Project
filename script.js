5c2c06d7c7dcd3faba4c22167d011f7fb8d11bcb
55746 <Lawhead.gavin@live.jobcorps.org>
2022-07-25 18:23:05 +0000

    need it to write on the page

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 const minerals = [
-  { gem: "Diamond", hardness: 1 },
-  { gem: "Ruby", hardness: 2 },
-  { gem: "Sapphire", hardness: 3 },
-  { gem: "Emerald", hardness: 4 },
-  { gem: "Amathyst", hardness: 5 },
-  { gem: "Amber", hardness: 6 },
+  { gem: "diamond", hardness: 1 },
+  { gem: "ruby", hardness: 2 },
+  { gem: "sapphire", hardness: 3 },
+  { gem: "emerald", hardness: 4 },
+  { gem: "amathyst", hardness: 5 },
+  { gem: "amber", hardness: 6 },
 ];
 
 // make a return funtion thjat returns the matching word from the array
 window.checkarray = function () {
   let searchinput = document.getElementById("searchinput");
-  let doc = document.getElementById("forum").innerHTML;
+  let doc = document.getElementById("answer").innerHTML;
   // i am calling the textg in the input
-  doc = "";
   for (let i = 0; i < minerals.length; i++) {
     // this is a for loop, looping the array
     if (searchinput.value == minerals[i].gem) {
       // if the search has the same text as the array
+      doc = searchinput.value + " was found in the array";
       console.log(searchinput.value + " was found in the array");
       // then console.log whats in the searchbar
     }
-    if (doc == "") {
-      doc = searchinput.value + " was found in the array";
-    }
   }
 };
+// Have to search through lowercase and uppercase
