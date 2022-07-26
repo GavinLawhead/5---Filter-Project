6aa957ed9afae514bd22177fc77f0e74aa4c9fc8
55746 <Lawhead.gavin@live.jobcorps.org>
2022-07-26 12:34:45 +0000

    need to test in a different program, its not working in github for some reason

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ const minerals = [
 ];
 
 // make a return funtion thjat returns the matching word from the array
-window.checkarray = function () {
+function checkarray() {
   let searchinput = document.getElementById("searchinput");
-  let doc = document.getElementById("answer").innerHTML;
+  let doc = document.getElementById("answer");
   // i am calling the textg in the input
   for (let i = 0; i < minerals.length; i++) {
     // this is a for loop, looping the array
     if (searchinput.value == minerals[i].gem) {
       // if the search has the same text as the array
-      doc = searchinput.value + " was found in the array";
+      doc.innerText = searchinput.value + " was found in the array";
       console.log(searchinput.value + " was found in the array");
       // then console.log whats in the searchbar
     }
   }
-};
+}
 // Have to search through lowercase and uppercase
