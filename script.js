c18e66f85b667535ac4b7d5281534bba4f11a575
55746 <Lawhead.gavin@live.jobcorps.org>
2022-07-28 17:13:24 +0000

    combineing arrays issue

--- script.js
+++ script.js
@@ -1,42 +1,65 @@
-const minerals = [
-  " Vesuvianit",
-  " Watermelon Tourmaline",
-  " Willemite",
-  " Zincite",
-  " Zircon",
-  " Zoisite",
-  " Zultanite",
-];
-const stones = [
-  " granite",
-  " obsidion",
-  " marbel",
-  " concrete",
-  " quartz",
-  " talc",
-  " river-rock",
-];
-const allarr = stones + minerals;
-function all() {
-  let searchinput3 = document.querySelector(".text");
-  // let string = JSON.stringify(minerals.gem);
-  searchinput3.innerText = allarr;
-  console.log(allarr);
-  // YOUR ALLLARR FUNCTION ISNT WORKING
+// const minerals = [
+//   " Vesuvianit",
+//   " Watermelon Tourmaline",
+//   " Willemite",
+//   " Zincite",
+//   " Zircon",
+//   " Zoisite",
+//   " Zultanite",
+// ];
+// const stones = [
+//   " granite",
+//   " obsidion",
+//   " marbel",
+//   " concrete",
+//   " quartz",
+//   " talc",
+//   " river-rock",
+// ];
+// const combine = [];
+// let allarr = combine.concat(minerals, stones);
+// function all() {
+//   let searchinput3 = document.querySelector(".text");
+//   // let string = JSON.stringify(minerals.gem);
+//   searchinput3.innerText = JSON.stringify(allarr);
+//   console.log(allarr);
+//   // YOUR ALLLARR FUNCTION ISNT WORKING
+// }
+// function gems() {
+//   let searchinput2 = document.querySelector(".text");
+//   // let string = JSON.stringify(minerals.gem);
+//   for (let i = 0; i < stones.length; i++) {
+//     searchinput2.innerText = stones;
+//     console.log(stones[i]);
+//   }
+// }
+// function stone() {
+//   let searchinput = document.querySelector(".text");
+//   // let string = JSON.stringify(minerals.gem);
+//   for (let i = 0; i < minerals.length; i++) {
+//     searchinput.innerText = minerals;
+//     console.log(minerals[i]);
+//   }
+// }
+let arr1 = [1,2,3]
+let arr2 = [4,5,6]
+let combine = arr1 + arr2
+function all(){
+let searchinput = document.querySelector(".text")
+searchinput.innerText = combine
 }
-function gems() {
+function gems(){
+  let searchinput2 = document.querySelector(".text");
+  // let string = JSON.stringify(minerals.gem);
+  for (let i = 0; i < stones.length; i++) {
+    searchinput2.innerText = stones;
+    console.log(stones[i]);
+}
+function minerals(){
   let searchinput = document.querySelector(".text");
   // let string = JSON.stringify(minerals.gem);
   for (let i = 0; i < minerals.length; i++) {
     searchinput.innerText = minerals;
-    console.log(minerals[i]);
-  }
+    console.log(minerals[i])
 }
-function stone() {
-  let searchinput2 = document.querySelector(".text");
-  // let string = JSON.stringify(minerals.gem);
-  for (let j = 0; j < stones.length; j++) {
-    searchinput2.innerText = stones;
-    console.log(stones[j]);
-  }
-}
+}}
