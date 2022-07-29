6f11a7a9f37a8c6c403acbc08b093954f70cccad
55746 <Lawhead.gavin@live.jobcorps.org>
2022-07-29 13:37:51 +0000

    need to work on project

--- script.js
+++ script.js
@@ -1,63 +1,58 @@
-// const minerals = [
-//   " Vesuvianit",
-//   " Watermelon Tourmaline",
-//   " Willemite",
-//   " Zincite",
-//   " Zircon",
-//   " Zoisite",
-//   " Zultanite",
-// ];
-// const stones = [
-//   " granite",
-//   " obsidion",
-//   " marbel",
-//   " concrete",
-//   " quartz",
-//   " talc",
-//   " river-rock",
-// ];
-// const combine = [];
-// let allarr = combine.concat(minerals, stones);
-// function all() {
-//   let searchinput3 = document.querySelector(".text");
-//   // let string = JSON.stringify(minerals.gem);
-//   searchinput3.innerText = JSON.stringify(allarr);
-//   console.log(allarr);
-//   // YOUR ALLLARR FUNCTION ISNT WORKING
-// }
-// function gems() {
-//   let searchinput2 = document.querySelector(".text");
-//   // let string = JSON.stringify(minerals.gem);
-//   for (let i = 0; i < stones.length; i++) {
-//     searchinput2.innerText = stones;
-//     console.log(stones[i]);
-//   }
-// }
-// function stone() {
-//   let searchinput = document.querySelector(".text");
-//   // let string = JSON.stringify(minerals.gem);
-//   for (let i = 0; i < minerals.length; i++) {
-//     searchinput.innerText = minerals;
-//     console.log(minerals[i]);
-//   }
-// }
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
-const combine = arr1.concat(arr2);
+const minerals = [
+  " Vesuvianit",
+  " Watermelon Tourmaline",
+  " Willemite",
+  " Zincite",
+  " Zircon",
+  " Zoisite",
+  " Zultanite",
+];
+const stones = [
+  " granite",
+  " obsidion",
+  " marbel",
+  " concrete",
+  " quartz",
+  " talc",
+  " river-rock",
+];
+const allarr = [
+  " granite",
+  " obsidion",
+  " marbel",
+  " concrete",
+  " quartz",
+  " talc",
+  " river-rock",
+  " Vesuvianit",
+  " Watermelon Tourmaline",
+  " Willemite",
+  " Zincite",
+  " Zircon",
+  " Zoisite",
+  " Zultanite",
+];
+const combine = [];
 function all() {
-  return combine;
+  let searchinput3 = document.querySelector(".text");
+  // let string = JSON.stringify(minerals.gem);
+  searchinput3.innerText = JSON.stringify(allarr);
+  console.log(allarr);
+  // YOUR ALLLARR FUNCTION ISNT WORKING
 }
 function gems() {
   let searchinput2 = document.querySelector(".text");
-  for (let i = 0; i < arr1.length; i++) {
-    searchinput2.innerText = arr1;
-    console.log(arr1[i]);
+  // let string = JSON.stringify(minerals.gem);
+  for (let i = 0; i < stones.length; i++) {
+    searchinput2.innerText = stones;
+    console.log(stones[i]);
   }
 }
 function stone() {
   let searchinput = document.querySelector(".text");
-  for (let i = 0; i < arr2.length; i++) {
-    searchinput.innerText = arr2;
-    console.log(arr2[i]);
+  // let string = JSON.stringify(minerals.gem);
+  for (let i = 0; i < minerals.length; i++) {
+    searchinput.innerText = minerals;
+    console.log(minerals[i]);
   }
 }
