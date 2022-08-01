f47cf92acf4edd369d005753da401d98694930e9
55746 <Lawhead.gavin@live.jobcorps.org>
2022-08-01 15:19:11 +0000

    GOT IT TO WORK AT LEAST

--- script.js
+++ script.js
@@ -1,60 +1,246 @@
-const allarr = [
-  " granite",
-  " obsidion",
-  " marbel",
-  " concrete",
-  " quartz",
-  " talc",
-  " river-rock",
-  " Vesuvianit",
-  " Watermelon Tourmaline",
-  " Willemite",
-  " Zincite",
-  " Zircon",
-  " Zoisite",
-  " Zultanite",
+const letterA = [" Ashley", " Aubrey", " Audrey", " Austin", " Autumn"];
+const letterB = [
+  " Barbara",
+  " Barry",
+  " Beau",
+  " Benjamin",
+  " Bernard",
+  " Beth",
 ];
-const minerals = [
-  " Vesuvianit",
-  " Watermelon Tourmaline",
-  " Willemite",
-  " Zincite",
-  " Zircon",
-  " Zoisite",
-  " Zultanite",
+const letterC = [
+  " Caitlin",
+  " Craig",
+  " Cristina",
+  " Crystal",
+  " Curtis",
+  " Cynthia",
 ];
-const stones = [
-  " granite",
-  " obsidion",
-  " marbel",
-  " concrete",
-  " quartz",
-  " talc",
-  " river-rock",
+const letterD = [" Douglas", " Drew", " Duane", " Dustin", " Dwayne", " Dylan"];
+const letterE = [" Ethan", " Eugene", " Eva", " Evan", " Evelyn"];
+const letterF = [
+  " Frances",
+  " Francis",
+  " Francisco",
+  " Frank",
+  " Franklin",
+  " Frederick",
 ];
+const letterG = [" Gloria", " Grace", " Grant", " Gregory", " Guadalupe"];
+const letterH = [" Hollie", " Holly", " Hope", " Howard", " Hunter"];
+const letterI = [" Ian", " India", " Irene", " Isaac", " Isaiah", " Israel"];
+const letterJ = [" Julianne", " Julie", " Julio", " Justin", " Justine"];
+const letterK = [" Kristy", " Krystal", " Krystle", " Kurt", " Kyle"];
+const letterL = [" Luis", " Luke", " Lydia", " Lyndsey", " Lynn"];
+const letterM = [" Mitchell", " Molly", " Monica", " Monique", " Morgan"];
+const letterN = [" Nikki", " Nina", " Noah", " Nora", " Norman"];
+const letterO = [" Olivia", " Omar", " Orlando", " Oscar"];
+const letterP = [" Philip", " Phillip", " Preston", " Priscilla"];
+const letterQ = [" Quentin", " Quinton"];
+const letterR = [" Roxanne", " Roy", " Ruben", " Ruby", " Russell"];
+const letterS = [" Summer", " Susan", " Suzanne", " Sylvia"];
+const letterT = [" Tristan", " Troy", " Tyler", " Tyrone", " Tyson"];
+const letterU = [" ulger", " unlrich", " unter"];
+const letterV = [
+  " Valerie",
+  " Vanessa",
+  " Vernon",
+  " Veronica",
+  " Victor",
+  " Victoria",
+  " Vincent",
+  " Virginia",
+];
+const letterW = [" Wade", " Walter", " Warren", " Wayne", " Wendy", " Wesley"];
+const letterX = [" Xavier"];
+const letterY = [" Yolanda", " Yvette", " Yvonne"];
+const letterZ = [" Zachary", " Zachery"];
+const allarr =
+  letterA +
+  letterB +
+  letterC +
+  letterD +
+  letterE +
+  letterF +
+  letterG +
+  letterH +
+  letterI +
+  letterJ +
+  letterK +
+  letterL +
+  letterM +
+  letterN +
+  letterO +
+  letterP +
+  letterQ +
+  letterR +
+  letterS +
+  letterT +
+  letterU +
+  letterV +
+  letterX +
+  letterY +
+  letterZ;
 let searchinput = document.querySelector(".text");
-searchinput = "";
-function all() {
-  let searchinput3 = document.querySelector(".text");
-  // let string = JSON.stringify(minerals.gem);
-  for (let i = 0; i < allarr.length; i++) {
-    searchinput3.innerText = allarr;
-    console.log(allarr[i]);
-  }
-}
-function gems() {
-  let searchinput2 = document.querySelector(".text");
-  // let string = JSON.stringify(minerals.gem);
-  for (let i = 0; i < stones.length; i++) {
-    searchinput2.innerText = stones;
-    console.log(stones[i]);
-  }
-}
-function stone() {
+searchinput.innerText = "sort the names by letter";
+function combined() {
   let searchinput = document.querySelector(".text");
-  // let string = JSON.stringify(minerals.gem);
-  for (let i = 0; i < minerals.length; i++) {
-    searchinput.innerText = minerals;
-    console.log(minerals[i]);
+  for (let i = 0; i < allarr.length; i++) {
+    searchinput.innerText = allarr;
+  }
+}
+function a() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterA.length; i++) {
+    searchinput.innerText = letterA;
+  }
+}
+function b() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterB.length; i++) {
+    searchinput.innerText = letterB;
+  }
+}
+function c() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterC.length; i++) {
+    searchinput.innerText = letterC;
+  }
+}
+function d() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterD.length; i++) {
+    searchinput.innerText = letterD;
+  }
+}
+function e() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterE.length; i++) {
+    searchinput.innerText = letterE;
+  }
+}
+function f() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterF.length; i++) {
+    searchinput.innerText = letterF;
+  }
+}
+function g() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterG.length; i++) {
+    searchinput.innerText = letterG;
+  }
+}
+function h() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterH.length; i++) {
+    searchinput.innerText = letterH;
+  }
+}
+function i() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterI.length; i++) {
+    searchinput.innerText = letterI;
+  }
+}
+function j() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterJ.length; i++) {
+    searchinput.innerText = letterJ;
+  }
+}
+function k() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterK.length; i++) {
+    searchinput.innerText = letterK;
+  }
+}
+function l() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterL.length; i++) {
+    searchinput.innerText = letterL;
+  }
+}
+function m() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterM.length; i++) {
+    searchinput.innerText = letterM;
+  }
+}
+function n() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterN.length; i++) {
+    searchinput.innerText = letterN;
+  }
+}
+function o() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterO.length; i++) {
+    searchinput.innerText = letterO;
+  }
+}
+function p() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterP.length; i++) {
+    searchinput.innerText = letterP;
+  }
+}
+function q() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterQ.length; i++) {
+    searchinput.innerText = letterQ;
+  }
+}
+function r() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterR.length; i++) {
+    searchinput.innerText = letterR;
+  }
+}
+function s() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterS.length; i++) {
+    searchinput.innerText = letterS;
+  }
+}
+function t() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterT.length; i++) {
+    searchinput.innerText = letterT;
+  }
+}
+function u() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterU.length; i++) {
+    searchinput.innerText = letterU;
+  }
+}
+function v() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterV.length; i++) {
+    searchinput.innerText = letterV;
+  }
+}
+function w() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterW.length; i++) {
+    searchinput.innerText = letterW;
+  }
+}
+function x() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterX.length; i++) {
+    searchinput.innerText = letterX;
+  }
+}
+function y() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterY.length; i++) {
+    searchinput.innerText = letterY;
+  }
+}
+function z() {
+  let searchinput = document.querySelector(".text");
+  for (let i = 0; i < letterZ.length; i++) {
+    searchinput.innerText = letterZ;
   }
 }
